82626d166f851d85955cebd6607b4763e5ab5740
xEnVrE <nicolapiga@gmail.com>
2016-06-15 15:32:24 +0200

    -Implement matrix_tranpose -Implement matrix_copy -Implement copy of operands in some matrix functions

--- matrix.c
+++ matrix.c
@@ -1,47 +1,85 @@
 #include "matrix.h"
+#include <stdio.h>
+
+//------------------------------------------------------------------------------
+//	Function matrix_copy
+//	copies the matrix 'src' into the matrix 'dst'
+//------------------------------------------------------------------------------
+static
+void	matrix_copy(int n, int m, float src[][m], float dst[][m])
+{
+int		i, j;
+
+	for (i = 0; i < n; i++)
+		for (j = 0; j < m; j++)
+			dst[i][j] = src[i][j];
+}
 
 //------------------------------------------------------------------------------
 //	Function matrix sum
-//	evaluates the sum of two matrices op1 and op2
-//	and put the result in res
+//	evaluates the sum of two matrices 'op1' and 'op2'
+//	and put the result in 'dst'
 //------------------------------------------------------------------------------
-void	matrix_sum(int n, int m, float op1[][m], float op2[][m], float res[][m])
+void	matrix_sum(int n, int m, float op1[][m], float op2[][m], float dst[][m])
 {
 int 	i, j;
 
 		for (i = 0; i < n; i++)
 			for (j = 0; j < m; j++)
-				res[i][j] = op1[i][j] + op2[i][j];
+				dst[i][j] = op1[i][j] + op2[i][j];
 }
 
 //------------------------------------------------------------------------------
 //	Function matrix_mul
-//	evaluates the product of two matrices op1 and op2
-//	and put the result in res
+//	evaluates the product of two matrices 'op1' and 'op2'
+//	and put the result in 'dst'
 //------------------------------------------------------------------------------
 void	matrix_mul(int op1_n, int op1_m, float op1[][op1_m],\
 					int op2_n, int op2_m, float op2[][op2_m],\
-			   		float res[op1_n][op2_m])
+			   		float dst[op1_n][op2_m])
 {
 int		i, j, k;
 float 	a_ij;
+float	op1_copy[op1_n][op1_m], op2_copy[op2_n][op2_m];
+
+		matrix_copy(op1_n, op1_m, op1, op1_copy);
+		matrix_copy(op2_n, op2_m, op2, op2_copy);
+
 		for (i = 0; i < op1_n; i++)
-			for (j = 0; j < op2_m; j++)
-			{
+			for (j = 0; j < op2_m; j++) {
 				a_ij = 0;
 				for (k = 0; k < op1_m; k++)
-					a_ij += op1[i][k] * op2[k][j];
-				res[i][j] = a_ij;
+					a_ij += op1_copy[i][k] * op2_copy[k][j];
+				dst[i][j] = a_ij;
 			}
 }
 
 //------------------------------------------------------------------------------
-//	Function matrix_3_det
-//	returns the determinant of a 3 by 3 matrix (closed form used)
+//	Function matrix_transpose
+//	evaluates the transpose of the matrix 'src'
+//	and put the result in 'dst'
 //------------------------------------------------------------------------------
+void	matrix_transpose(int n, int m, float src[][m], float dst[][n])
+{
+int 	i, j;
+float	src_copy[n][m];
+
+		matrix_copy(n, m, src, src_copy);
+
+		for (i = 0; i < n; i++)
+			for (j = 0; j < m; j++)
+				dst[j][i] = src_copy[i][j];
+}
+
+//------------------------------------------------------------------------------
+//	Function matrix_3_det
+//	returns the determinant of the 3 by 3 matrix 'm' (closed form used)
+//------------------------------------------------------------------------------
+static
 float	matrix_3_det(float m[3][3])
 {
 float	result;
+
 		result = m[0][0] * (m[2][2] * m[1][1] - m[2][1] * m[1][2]) -\
 				m[1][0] * (m[2][2] * m[0][1] - m[2][1] * m[0][2]) +\
 				m[2][0] * (m[1][2] * m[0][1] - m[1][1] * m[0][2]);
@@ -51,36 +89,41 @@ float	result;
 
 //------------------------------------------------------------------------------
 //	Function matrix_3_adj
-//	evaluates the adjoint of a 3 by 3 matrix (closed form used)
-//	and put the result in res
+//	evaluates the adjoint of the 3 by 3 matrix 'src' (closed form used)
+//	and put the result in 'dst'
 //------------------------------------------------------------------------------
-void	matrix_3_adj(float m[3][3], float res[3][3])
+static
+void	matrix_3_adj(float src[3][3], float dst[3][3])
 {
-		res[0][0] = m[2][2] * m[1][1] - m[2][1] * m[1][2];
-		res[0][1] = -(m[2][2] * m[0][1] - m[2][1] * m[0][2]);
-		res[0][2] = m[1][2] * m[0][1] - m[1][1] * m[0][2];
-		res[1][0] = -(m[2][2] * m[1][0] - m[2][0] * m[1][2]);
-		res[1][1] = m[2][2] * m[0][0] - m[2][0] * m[0][2];
-		res[1][2] = -(m[1][2] * m[0][0] - m[1][0] * m[0][2]);
-		res[2][0] = m[2][1] * m[1][0] - m[2][0] * m[1][1];
-		res[2][1] = -(m[2][1] * m[0][0] - m[2][0] * m[0][1]);
-		res[2][2] = m[1][1] * m[0][0] - m[1][0] * m[0][1];
+float	m[3][3];
+
+		matrix_copy(3, 3, src, m);
+
+		dst[0][0] = m[2][2] * m[1][1] - m[2][1] * m[1][2];
+		dst[0][1] = -(m[2][2] * m[0][1] - m[2][1] * m[0][2]);
+		dst[0][2] = m[1][2] * m[0][1] - m[1][1] * m[0][2];
+		dst[1][0] = -(m[2][2] * m[1][0] - m[2][0] * m[1][2]);
+		dst[1][1] = m[2][2] * m[0][0] - m[2][0] * m[0][2];
+		dst[1][2] = -(m[1][2] * m[0][0] - m[1][0] * m[0][2]);
+		dst[2][0] = m[2][1] * m[1][0] - m[2][0] * m[1][1];
+		dst[2][1] = -(m[2][1] * m[0][0] - m[2][0] * m[0][1]);
+		dst[2][2] = m[1][1] * m[0][0] - m[1][0] * m[0][1];
 }
 
 //------------------------------------------------------------------------------
 //	Function matrix_3_inv
-//	evaluates the inverse of a 3 by 3 matrix (closed form used)
-//	and put the result in res
+//	evaluates the inverse of the 3 by 3 matrix 'matrix' (closed form used)
+//	and put the result in 'dst'
 //------------------------------------------------------------------------------
-void	matrix_3_inv(float matrix[3][3], float res[3][3])
+void	matrix_3_inv(float matrix[3][3], float dst[3][3])
 {
 int		i, j;
 float	determinant;
 
-		matrix_3_adj(matrix, res);
 		determinant = matrix_3_det(matrix);
+		matrix_3_adj(matrix, dst);
 
 		for (i = 0; i < 3; i++)
 			for (j = 0; j < 3; j++)
-				res[i][j] /= determinant;
+				dst[i][j] /= determinant;
 }
